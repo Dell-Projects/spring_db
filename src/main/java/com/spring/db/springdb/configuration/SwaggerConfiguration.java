67ff91c2a3ff1bbb4c77953073e33c7601716005
LuisPereir4 <49247322+LuisPereir4@users.noreply.github.com>
2019-12-06 20:05:39 -0300

    Swagger

--- src/main/java/com/spring/db/springdb/configuration/SwaggerConfiguration.java
+++ src/main/java/com/spring/db/springdb/configuration/SwaggerConfiguration.java
@@ -1,10 +1,45 @@
 package com.spring.db.springdb.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@RestController
 @EnableSwagger2
 @Configuration
 public class SwaggerConfiguration {
 
+    @Bean
+    public Docket greetingApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.spring.db.springdb"))
+                .build()
+                .apiInfo(metaData());
+
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Spring DB")
+                .description("\"REST API for JPA and H2 study\"")
+                .version("1.0.0")
+                .license("Luis Felipe Pereira")
+                .licenseUrl("https://www.linkedin.com/in/luispereir4/")
+                .build();
+    }
+
+    @GetMapping("/")
+    public ModelAndView redirectToSwaggerUi(){
+        return new ModelAndView("redirect:swagger-ui.html");
+    }
 }
